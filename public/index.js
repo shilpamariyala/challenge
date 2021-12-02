8032a77921b5ed7814ba321282e0f97bcd94582e
shilpak08 <mariyalashilpa3@gmail.com>
2021-12-01 21:18:52 -0600

    Add Age demographic table

--- public/index.js
+++ public/index.js
@@ -15,4 +15,24 @@ async function getUsers() {
     document.getElementById("age_table").style.display='none';
 }
 
+
+async function getAgeDemographicInfo(item) {
+    const res = await fetch(`http://localhost:3000/users/age/${item}`);
+    const result = await res.json();
+    document.getElementById("age_table").style.display='table';
+    const tableBody = document.getElementById('age_table_body');
+    tableBody.innerHTML = '';
+    const ageKeys = Object.keys(result);
+    for(let i=0; i<ageKeys.length; i++) {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        td1.appendChild(document.createTextNode(ageKeys[i]));
+        td2.appendChild(document.createTextNode(result[ageKeys[i]]));
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tableBody.appendChild(tr);
+    }
+}
+
 getUsers();
