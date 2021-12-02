7049a07cfdee66d947de608e98e309564d7f9a51
shilpak08 <mariyalashilpa3@gmail.com>
2021-12-01 21:08:11 -0600

    Add index file for the deafult route

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@
 const express = require('express');
 const app = express();
 const registerRoutes = require('./routes');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
 
 // server config
 const port = process.env.PORT || 3000;
+
+app.use('/', express.static(publicPath));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 // register routes
 registerRoutes(app);
